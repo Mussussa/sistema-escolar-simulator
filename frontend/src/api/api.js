6f6252f7f9d9bde6c885c7a7a48767d2336c9c33
Mussussa <xismaelchaibo@gmail.com>
2026-02-16 14:26:54 +0200

    vai

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Troca o IP por localhost
-  baseURL: import.meta.env.VITE_API_URL, 
+  baseURL: import.meta.env.VITE_API_URL + '/api', 
   withCredentials: true // Isto é obrigatório para enviar/receber o cookie
 });
 
